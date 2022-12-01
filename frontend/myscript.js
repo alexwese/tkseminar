22221ecb6d2b04e5a7d6ef7c024148dd95210b9c
TomH147 <t.dizzle797@gmail.com>
2022-12-01 23:18:32 +0100

    New Nodes

--- frontend/myscript.js
+++ frontend/myscript.js
@@ -1,30 +1,40 @@
-var trigger1 = 0.5;
+var base1 = 200;
+var base1 = 259.85;
+var base1 = 5850;
+
+
+var change1 = 0;
+var change2 = 0;
+var change3 = 0;
+
+var prob1 = 100;
+var prob2 = 100;
+var prob3 = 100;
+
 var trigger1Sign = 1;
-var prob1 = 0.1;
-var trigger2 = 0.5;
 var trigger2Sign = 1;
-var prob2 = 0.1;
+var trigger3Sign = 1;
 
+var coeff1 = 0.5;
+var coeff2 = 4.1;
+var coeff3 = 0.13;
 
+var aluPrice = 3245.79
 
-var re2 = 0.1;
 
 var myLine1 = new LeaderLine(
     document.getElementById('trigger1'),
     document.getElementById('re1')
 );
 
-myLine1.setOptions({color: '#89B0AE', middleLabel: LeaderLine.pathLabel(String(trigger1))})
+myLine1.setOptions({color: '#89B0AE', middleLabel: LeaderLine.pathLabel(String(coeff1))})
 function updateWeight1(val) {
     myLine1.middleLabel = LeaderLine.pathLabel(String(val)); 
     trigger1 = val;
     updateRE1();
 }
 
-function updateProb1(val) {
-    prob1 = val;
-    updateRE1();
-}
+
 
 function changeSign1(checked) {
     if (checked) {
@@ -48,22 +58,12 @@ function updateRE1() {
 }
 
 var myLine2 = new LeaderLine(
-      document.getElementById('trigger2'),
-      document.getElementById('re1')
+      document.getElementById('re1'),
+      document.getElementById('price')
 );
 
-myLine2.setOptions({color: '#89B0AE', middleLabel: LeaderLine.pathLabel(String(trigger2))})
+myLine2.setOptions({color: '#89B0AE', middleLabel: LeaderLine.pathLabel(String(coeff2))})
 
-function updateWeight2(val) {
-    myLine2.middleLabel = LeaderLine.pathLabel(String(val)); 
-    trigger2 = val;
-    updateRE1();
-}
-
-function updateProb2(val) {
-    prob2 = val;
-    updateRE1();
-}
 
 function changeSign2(checked) {
     if (checked) {
@@ -77,15 +77,21 @@ function changeSign2(checked) {
 }
 
 var myLine3 = new LeaderLine(
-    document.getElementById('re1'),
-    document.getElementById('price')
-);
-
-myLine3.setOptions({color: '#89B0AE', middleLabel: LeaderLine.pathLabel("0.3")})
-
-var myLine4 = new LeaderLine(
     document.getElementById('re2'),
     document.getElementById('price')
 );
-  
-myLine4.setOptions({color: '#89B0AE', middleLabel: LeaderLine.pathLabel("0.7")})
+
+myLine3.setOptions({color: '#89B0AE', middleLabel: LeaderLine.pathLabel(String(coeff3))})
+
+function changeSign3(checked) {
+    if (checked) {
+        trigger3Sign = -1;
+        myLine3.color = '#FFAF79';
+    } else {
+        trigger3Sign = 1;
+        myLine3.color = '#89B0AE';
+    }
+    updateRE1();
+}
+
+
