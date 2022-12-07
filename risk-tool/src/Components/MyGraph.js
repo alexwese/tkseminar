8d7c4ae0b013b859012d52f07063dcff3018456c
Don Diephaus <don.diephaus@gmx.com>
2022-12-07 17:34:15 +0100

    d3 tree

--- risk-tool/src/Components/MyGraph.js
+++ risk-tool/src/Components/MyGraph.js
@@ -1,10 +1,51 @@
 import { Container } from "react-bootstrap";
+import React from 'react';
+import Tree from 'react-d3-tree';
+
+const orgChart = {
+  name: 'CEO',
+  children: [
+    {
+      name: 'Manager',
+      attributes: {
+        department: 'Production',
+      },
+      children: [
+        {
+          name: 'Foreman',
+          attributes: {
+            department: 'Fabrication',
+          },
+          children: [
+            {
+              name: 'Worker',
+            },
+          ],
+        },
+        {
+          name: 'Foreman',
+          attributes: {
+            department: 'Assembly',
+          },
+          children: [
+            {
+              name: 'Worker',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
 
 function MyGraph(props) {
   return (
     <>
       <div className="text-center">
-        <h5>Don Plzezez add here!!!</h5>
+        <div id="treeWrapper" style={{ width: '50em', height: '20em' }}>
+            <Tree data={orgChart} />
+        </div>
       </div>
     </>
   );
