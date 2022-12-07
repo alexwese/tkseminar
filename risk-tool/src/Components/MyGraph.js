9427842cd765332a54431c0ef5d26bb322fbdb51
TomH147 <t.dizzle797@gmail.com>
2022-12-07 19:55:09 +0100

    New Cards for Nodes

--- risk-tool/src/Components/MyGraph.js
+++ risk-tool/src/Components/MyGraph.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Tree from "react-d3-tree";
 
-import orgChartJson from "../data/org-chart.json";
+import orgChartJson from "../data/risk_data.json";
 import { useCenteredTree } from "./myGraphHelpers";
 import "./myGraph.css";
+
+//Bootstrap
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 const renderForeignObjectNode = ({
   nodeDatum,
@@ -14,21 +20,41 @@ const renderForeignObjectNode = ({
     <circle r={15}></circle>
     {/* `foreignObject` requires width & height to be explicitly set. */}
     <foreignObject {...foreignObjectProps}>
-      <div style={{ border: "1px solid black", backgroundColor: "#dedede" }}>
+      <Card style={{ width: "18rem" }}>
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            <h4>{nodeDatum.name}</h4>
+          </ListGroup.Item>
+          <ListGroup.Item>Base Value: </ListGroup.Item>
+          <ListGroup.Item>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="inputGroup-sizing-default">
+                Default
+              </InputGroup.Text>
+              <Form.Control
+                aria-label="Default"
+                aria-describedby="inputGroup-sizing-default"
+              />
+            </InputGroup>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
+
+      {/* <div style={{ border: "1px solid black", backgroundColor: "#dedede" }}>
         <h3 style={{ textAlign: "center" }}>{nodeDatum.name}</h3>
         {nodeDatum.children && (
           <button style={{ width: "100%" }} onClick={toggleNode}>
             {nodeDatum.__rd3t.collapsed ? "Expand" : "Collapse"}
           </button>
         )}
-      </div>
+      </div> */}
     </foreignObject>
   </g>
 );
 
 function MyGraph(props) {
   const [translate, containerRef] = useCenteredTree();
-  const nodeSize = { x: 200, y: 200 };
+  const nodeSize = { x: 500, y: 500 };
   const foreignObjectProps = { width: nodeSize.x, height: nodeSize.y, x: 20 };
   return (
     <>
@@ -41,7 +67,7 @@ function MyGraph(props) {
             renderCustomNodeElement={(rd3tProps) =>
               renderForeignObjectNode({ ...rd3tProps, foreignObjectProps })
             }
-            orientation="vertical"
+            orientation="horizontal"
           />
         </div>
       </div>
