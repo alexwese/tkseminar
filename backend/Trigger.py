51687cf817e4c5dce1a02dbf52e1096d362ad7ce
Amnah199 <amnahkhan.ak@gmail.com>
2022-12-08 23:02:48 +0100

    Delete backend directory

--- backend/Trigger.py
+++
@@ -1,9 +0,0 @@
-class Trigger:
-    def __init__(self, probability, impact, edge_weight):
-        self.probability = probability
-        self.impact = impact
-        self.weight = edge_weight
-        self.trigger_risk = self.calculate_risk()
-
-    def calculate_risk(self):
-        return self.impact * self.probability * self.weight
