d501fdd6e3b4014feba9de072a59c35b96cf1fe4
Amna <amnahkhan.ak@gmail.com>
2022-12-10 14:41:09 +0100

    Changed api as per new json

--- Backend-New/api.py
+++ Backend-New/api.py
@@ -2,6 +2,7 @@ from flask import Flask,request,jsonify
 import pandas as pd
 import logging
 from Node import Node
+import json
 from main import *
 
 app = Flask(__name__)
@@ -10,6 +11,8 @@ logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:
     level=logging.INFO)
 
 @app.route('/calculate_price', methods=['POST'])
+
+
 
 def calculate_price():
     nodes_list = []
@@ -21,6 +24,7 @@ def calculate_price():
 
     for i in nodes:
         nodes_list.append(Node(i['node_id'], i['name'], i['base_val'], i['coefficient'], i['input_nodes']))
+
 
     for obj in nodes_list:
         node_objs.append(obj)
@@ -42,6 +46,35 @@ def calculate_price():
         if(i.name == "aluminium price"):
             result = i.expected_val
     return jsonify(result)
+
+@app.route('/build_network', methods=['GET'])
+
+def build_network():
+    base_network = json.load(open("risk_data.json"))
+    return base_network
+
+@app.route('/calculate_result', methods=['POST'])
+
+def  calculate_result():
+    content = request.json
+    logging.info(content)
+    nodes = content['children']
+    flag = False
+
+
+    for i in nodes:
+        if i["attributes"]['expected_change'] != None:
+            flag = True
+        if "children" in i:
+            for j in i['children']:
+                if j["attributes"]["expected_change"] != None:
+                    flag = True
+
+    if flag == False:
+        return jsonify(content)
+    else:
+        pass
+
 
 if __name__ == '__main__':
     app.run(host='localhost',port=8080)
